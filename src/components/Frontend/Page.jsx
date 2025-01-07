668f01335307ab65568a724eb127b77ae626e624
mdalmamungalib <mdalmamungalib3929@gmail.com>
2025-01-07 18:05:08 +0600

    clone add

--- src/components/Frontend/Page.jsx
+++ src/components/Frontend/Page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = () => {
     return (
         <div>
-            <h1>Main Page Here</h1>
+            <h1>Main Page Here is clone</h1>
         </div>
     );
 };
